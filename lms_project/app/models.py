0833a54d050f0736cb0f7038de9b51e436c7368e
SheetalJade2019 <sheetal.jade93@gmail.com>
2022-09-12 16:32:09 +0530

    database models added, mysql connection done

--- lms_project/app/models.py
+++ lms_project/app/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+from email.policy import default
+from tkinter import CASCADE
+from turtle import color
+from djongo import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# class User(AbstractUser):
+    # user_id = models.AutoField(primary_key=True)
+    # email = models.EmailField(unique=True)
+    
+    # def __str__(self) :
+    #     return str(self.user_id)
+
+    # def __str__(self) -> str:
+    #     return super().__str__(self.email)
+
+class Book(models.Model):
+    book_id = models.AutoField(primary_key=True,null=False)
+    book_name = models.CharField(max_length=200,unique=True)
+    book_price = models.DecimalField(max_digits=10, decimal_places=2)
+    book_author = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
--- lms_project/lms_project/settings.py
+++ lms_project/lms_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +74,24 @@ WSGI_APPLICATION = 'lms_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'keywordio_db',
+        'USER' : 'root',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'POST': 3306
     }
 }
-
+# superuser  - admin, admin, admin@gmail.com
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
